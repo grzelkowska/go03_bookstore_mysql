d9a530fdbafbf4d2d9804aa6085742fdb92add4d
Phillip <philliplee515@gmail.com>
2022-08-31 14:49:56 +0900

    models/book.go

---
+++ pkg/models/book.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"github.com/grzelkowska/11projects/03go_bookstore_mysql/pkg/config"
+	// "github.com/grzelkowska/go03_bookstore_mysql/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
